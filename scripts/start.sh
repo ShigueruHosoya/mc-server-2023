fc41565ed2e3bec96b604a0e68d76af561e793da
mc-server <mc@mc.com>
2023-06-22 21:08:09 +0000

    Added and deleted old files

---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 'Starting Minecraft Server!';
+screen -S mc-server -dm java -Xmx14G -Xms4G -XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150 -jar ../server.jar --port 25565 --nogui;
